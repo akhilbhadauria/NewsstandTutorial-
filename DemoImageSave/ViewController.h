4f4ec6779916e4c2fa15b38f8dbd282c9678f0c1
Shorhashi <admin@Shorhashi.local>
2011-11-17 11:05:38 +0530

    Initial Commit

---
+++ DemoImageSave/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoImageSave
+//
+//  Created by admin on 17/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DemoImageSaveTests/DemoImageSaveTests.m
@@ -0,0 +1,32 @@
+//
+//  DemoImageSaveTests.m
+//  DemoImageSaveTests
+//
+//  Created by admin on 17/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DemoImageSaveTests.h"
+
+@implementation DemoImageSaveTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DemoImageSaveTests");
+}
+
+@end
